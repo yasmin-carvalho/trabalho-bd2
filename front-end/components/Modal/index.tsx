e027a57222129f20505d07cd115653f07b586529
rodrigodsluz <rodrigodsluz@gmail.com>
2021-12-11 14:32:32 -0300

    fix: date formats

--- front-end/components/Modal/index.tsx
+++ front-end/components/Modal/index.tsx
@@ -1,9 +1,18 @@
+import moment from "moment";
+
 const Modal = ({ searchData, setSearchData }) => {
   const modal = document.getElementById("myModal");
   window.onclick = function (event) {
     if (event.target == modal) {
       setSearchData(null);
     }
+  };
+
+  const isDate = (_date) => {
+    const _regExp = new RegExp(
+      "^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(.[0-9]+)?(Z)?$"
+    );
+    return _regExp.test(_date);
   };
 
   return (
@@ -32,9 +41,16 @@ const Modal = ({ searchData, setSearchData }) => {
                 <tbody>
                   {searchData.map((item) => (
                     <tr key={item.id}>
-                      {Object.values(item).map((val) => (
-                        <td>{val}</td>
-                      ))}
+                      {Object.values(item).map((val: any) => {
+                        console.log(val);
+                        return (
+                          <td>
+                            {isDate(val)
+                              ? moment(val).format("MM/DD/YYYY")
+                              : val}
+                          </td>
+                        );
+                      })}
                     </tr>
                   ))}
                 </tbody>
